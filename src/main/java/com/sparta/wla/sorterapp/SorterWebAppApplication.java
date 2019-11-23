29c8e937efd6a3be0d8e5cebd5218a0314980499
Andy <Andy@iMac-2.local>
2019-11-23 20:54:04 +0000

    First commit

---
+++ src/main/java/com/sparta/wla/sorterapp/SorterWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.wla.sorterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorterWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorterWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/wla/sorterapp/SorterWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.wla.sorterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorterWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
